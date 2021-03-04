7af41064efa32e7d1c2fc56f938327797b878f3c
eason <cheng.yi@capitalcrux.com>
2021-03-04 11:04:09 +0800

    UE

--- src/pages/RuleDefinition/RuleLegend/shape/mindNode.js
+++ src/pages/RuleDefinition/RuleLegend/shape/mindNode.js
@@ -1,5 +1,6 @@
 const WRAPPER_CLASS_NAME = 'node-wrapper';
 const WRAPPER_STROKE = 'rgba(0, 0, 0, 0.1)';
+
 export default G6 => {
   G6.registerNode(
     'mind-node',
@@ -91,7 +92,7 @@ export default G6 => {
             y: rectBBox.maxY - 40,
             text: parentId ? rank : '',
             fontSize: 14,
-            fill: 'rgba(0, 0, 0, 0.45)',
+            fill: '#722ed1',
             opacity: 0.85,
           },
         });
@@ -105,7 +106,7 @@ export default G6 => {
               radius: 8,
               width: 16,
               height: 16,
-              stroke: 'rgba(0, 0, 0, 0.25)',
+              stroke: collapsed ? '#91d5ff' : 'rgba(0, 0, 0, 0.25)',
               cursor: 'pointer',
               fill: '#fff',
             },
@@ -117,13 +118,13 @@ export default G6 => {
           group.addShape('text', {
             attrs: {
               x: rectConfig.width / 2,
-              y: collapsed ? 0.5 : -1,
-              fontSize: collapsed ? 10 : 18,
+              y: 0,
+              fontSize: collapsed ? 14 : 18,
               textAlign: 'center',
               textBaseline: 'middle',
               text: collapsed ? cfg.children.length : '-',
               cursor: 'pointer',
-              fill: 'rgba(0, 0, 0, 0.25)',
+              fill: collapsed ? '#2db7f5' : 'rgba(0, 0, 0, 0.45)',
             },
             name: 'collapse-text',
             modelId: cfg.id,
@@ -142,18 +143,32 @@ export default G6 => {
         const states = item.getStates();
         const nodeData = item.getModel();
         if (name === 'collapse') {
+          const collapseBack = group.find(e => e.get('name') === 'collapse-back');
           const collapseText = group.find(e => e.get('name') === 'collapse-text');
+          if (collapseBack) {
+            if (!value) {
+              collapseBack.attr({
+                fill: '#fff',
+                stroke: 'rgba(0, 0, 0, 0.25)',
+              });
+            } else {
+              collapseBack.attr({
+                fill: '#e6f7ff',
+                stroke: '#91d5ff',
+              });
+            }
+          }
           if (collapseText) {
             if (!value) {
               collapseText.attr({
                 text: '-',
-                y: -1,
                 fontSize: 18,
+                fill: 'rgba(0, 0, 0, 0.45)',
               });
             } else {
               collapseText.attr({
-                fontSize: 10,
-                y: 0.5,
+                fill: '#2db7f5',
+                fontSize: 14,
                 text: nodeData.children.length,
               });
             }
